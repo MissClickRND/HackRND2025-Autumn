b846a7abb46a83465ccc88f813929c7b21286d98
THE-G0OSE <nikitos54345@gmail.com>
2025-10-25 03:22:34 +0300

    ux fix

--- src/widgets/navButton/ui/NavButton.tsx
+++ src/widgets/navButton/ui/NavButton.tsx
@@ -1,22 +1,49 @@
-import { ActionIcon, Center, Flex, Typography } from '@mantine/core'
-import { FC, ReactNode } from 'react'
-import { NavLink } from 'react-router-dom'
-import {motion} from 'motion/react'
-import { navButtonTitleVar } from '../../../layouts/MainLayout/components/animations'
+import { ActionIcon, Center, Flex, Typography } from "@mantine/core";
+import { FC, ReactNode } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { motion } from "motion/react";
+import { navButtonTitleVar } from "../../../layouts/MainLayout/components/animations";
 
 interface IProps {
-    to: string,
-    title: string, 
-    icon: ReactNode,
+  to: string;
+  title: string;
+  icon: ReactNode;
 }
 
-const NavButton: FC<IProps> = ({to, title, icon}) => {
+const NavButton: FC<IProps> = ({ to, title, icon }) => {
+  const location = useLocation();
   return (
     <Flex align={"center"}>
-        <NavLink style={{color: "black"}} to={to}><ActionIcon color='var(--navbar-button-color)' size={50} >{icon}</ActionIcon></NavLink>
-        <Center variants={navButtonTitleVar} component={motion.div} style={{position: "absolute", left: 80, borderRadius: ".5rem", pointerEvents: 'none'}} py={7} px={14} bg='var(--navbar-button-color)'><Typography>{title}</Typography></Center>
+      <NavLink style={{ color: "black" }} to={to}>
+        <ActionIcon
+          style={{ pointerEvents: to === location.pathname ? "none" : "unset" }}
+          color={
+            to === location.pathname
+              ? "lightgray"
+              : "var(--navbar-button-color)"
+          }
+          size={50}
+        >
+          {icon}
+        </ActionIcon>
+      </NavLink>
+      <Center
+        variants={navButtonTitleVar}
+        component={motion.div}
+        style={{
+          position: "absolute",
+          left: 80,
+          borderRadius: ".5rem",
+          pointerEvents: "none",
+        }}
+        py={7}
+        px={14}
+        bg="var(--navbar-button-color)"
+      >
+        <Typography>{title}</Typography>
+      </Center>
     </Flex>
-  )
-}
+  );
+};
 
-export default NavButton
+export default NavButton;
